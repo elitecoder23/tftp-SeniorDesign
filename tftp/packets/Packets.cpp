9404b71b313d5fc1bbf35d8c5de634b25641ae77
Thomas Vogt <thomas@thomas-vogt.de>
2022-05-26 23:25:00 +0200

    Add const

--- tftp/packets/Packets.cpp
+++ tftp/packets/Packets.cpp
@@ -16,7 +16,7 @@
 
 namespace Tftp::Packets {
 
-std::string_view TftpOptions_name( KnownOptions option ) noexcept
+std::string_view TftpOptions_name( const KnownOptions option ) noexcept
 {
   switch ( option )
   {
